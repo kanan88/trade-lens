4296c2b920012bbbc1675240c0724b699b7ef938
Kanan Farzaliyev <kanan.farzali@gmail.com>
2025-09-28 23:44:40 +0200

    implement header

---
+++ app/(root)/page.tsx
@@ -0,0 +1,5 @@
+const HomePage = () => {
+  return <div className="flex min-h-screen home-wrapper">Home</div>
+}
+
+export default HomePage
---
+++ components/Header.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+const Header = () => {
+  return (
+    <header className="sticky top-0 header">
+      <div className="container header-wrapper">
+        <Link href="/">
+          <Image
+            src="/assets/icons/logo.svg"
+            alt="TradeLens"
+            width={32}
+            height={32}
+            className="size-8 w-auto cursor-pointer"
+          />
+        </Link>
+        <nav className="hidden sm:block">{/* NavItems */}</nav>
+        {/* UserDropdown */}
+      </div>
+    </header>
+  )
+}
+
+export default Header
