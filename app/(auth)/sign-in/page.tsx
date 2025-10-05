44219738692baa3a0c4c9b0799515fb8133a6066
Kanan Farzaliyev <kanan.farzali@gmail.com>
2025-10-05 21:38:01 +0200

    implement sign in page withing ui and layout context

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,5 +1,74 @@
+'use client'
+
+import { useForm } from 'react-hook-form'
+
+import FooterLink from '@/components/forms/FooterLink'
+import InputField from '@/components/forms/InputField'
+import { Button } from '@/components/ui/button'
+
 const SignIn = () => {
-  return <div>SignIn</div>
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting }
+  } = useForm<SignInFormData>({
+    defaultValues: {
+      email: '',
+      password: ''
+    },
+    mode: 'onBlur'
+  })
+
+  const onSubmit = async (data: SignInFormData) => {
+    try {
+      console.log('Form Data: ', data)
+    } catch (e) {
+      console.error('Submission error: ', e)
+    }
+  }
+
+  return (
+    <>
+      <h1 className="form-title">Welcome back</h1>
+
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
+        <InputField
+          name="email"
+          label="Email"
+          register={register}
+          placeholder="joedoe@email.com"
+          error={errors.email}
+          validation={{ required: 'Email is required', pattern: /^\S+@\S+$/i }}
+          disabled={isSubmitting}
+        />
+
+        <InputField
+          name="password"
+          label="Password"
+          type="password"
+          register={register}
+          placeholder="Enter your password"
+          error={errors.password}
+          validation={{ required: 'Password is required', minLength: 8 }}
+          disabled={isSubmitting}
+        />
+
+        <Button
+          type="submit"
+          disabled={isSubmitting}
+          className="yellow-btn w-full mt-5"
+        >
+          {isSubmitting ? 'Signing In...' : 'Start Your Investing Journey'}
+        </Button>
+
+        <FooterLink
+          text="Don't have an account?"
+          linkText="Sign Up"
+          href="/sign-up"
+        />
+      </form>
+    </>
+  )
 }
 
 export default SignIn
