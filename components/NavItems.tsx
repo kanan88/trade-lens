54fe91a232f5006244dc0a78aa639b5930344488
Kanan Farzaliyev <kanan.farzali@gmail.com>
2025-10-14 21:47:24 +0200

    implement search component

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -3,6 +3,7 @@
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 
+import SearchCommand from '@/components/SearchCommand'
 import { NAV_ITEMS } from '@/lib/constants'
 
 const NavItems = () => {
@@ -16,18 +17,30 @@ const NavItems = () => {
 
   return (
     <ul className="flex flex-col sm:flex-row p-2 gap-3 sm:gap-10 font-medium">
-      {NAV_ITEMS.map(({ href, label }) => (
-        <li key={href}>
-          <Link
-            href={href}
-            className={`hover:text-yellow-500 transition-colors ${
-              isActive(href) ? 'text-gray-100' : ''
-            }`}
-          >
-            {label}
-          </Link>
-        </li>
-      ))}
+      {NAV_ITEMS.map(({ href, label }) => {
+        if (label === 'Search')
+          return (
+            <li key="search-trigger">
+              <SearchCommand
+                renderAs="text"
+                label="Search"
+                initialStocks={[]}
+              />
+            </li>
+          )
+        return (
+          <li key={href}>
+            <Link
+              href={href}
+              className={`hover:text-yellow-500 transition-colors ${
+                isActive(href) ? 'text-gray-100' : ''
+              }`}
+            >
+              {label}
+            </Link>
+          </li>
+        )
+      })}
     </ul>
   )
 }
